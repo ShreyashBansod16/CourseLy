8d0a37877d198d4c0ed7ee4f6c1d42e1ddb24152
Abusha Ansari <msaif8747@gmail.com>
2025-02-15 18:44:08 +0530

    updated logout logic

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -5,11 +5,13 @@ export async function GET() {
   const response = NextResponse.redirect(new URL("/user/login", process.env.NEXT_PUBLIC_BASE_URL));
 
   // Manually clear authentication cookie
-  response.cookies.set("next-auth.session-token", "", {
-    path: "/",
-    expires: new Date(0), // Expire immediately
-  });
+  // response.cookies.set("next-auth.session-token", "", {
+  //   path: "/",
+  //   expires: new Date(0), // Expire immediately
+  // });
+
+  response.cookies.delete("next-auth.session-token");
   redirect("/user/login");
 
-  return response;
+  // return response;
 }
